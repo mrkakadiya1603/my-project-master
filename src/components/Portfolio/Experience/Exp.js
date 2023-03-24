fd46a53f2a255183c558aa14ef612ebcd5a23ae8
mrkakadiya1603 <107118287+mrkakadiya1603@users.noreply.github.com>
2023-03-24 18:42:16 +0530

    minor changes

--- src/components/Portfolio/Experience/Exp.js
+++ src/components/Portfolio/Experience/Exp.js
@@ -1,53 +1,91 @@
-import React from "react";
-import { expData } from "./expData";
+// import React from "react";
+// import { expData } from "./expData";
 
-import "./Exp.css";
+// import "./Exp.css";
 
-const Exp = () => {
-    return (
-        <div id="experience" className="expContainer">
-            <div className="headerContainer">
-                <div className="header">
+// const Exp = () => {
+//     return (
+//         <div id="experience" className="expContainer">
+//             <div className="headerContainer">
+//                 <div className="header">
                     
-                    <p className="headerTitle" >Experience</p>
-                </div>
-                <div className="divider" style={{width: "255px"}}></div>
-            </div>
-            <div className="expContentContainer">
-                {
-                    expData.map((exp) => (
-                        <div className="expCard">
-                            <div className="expCardContent">
-                                <div className="expCardFront">
-                                    <img style={{objectFit: "contain", width: "55%", margin: "20px"}} src={exp.employerImg.img} alt="" />
-                                    <div style={{fontFamily: "'Abril Fatface', cursive", fontSize: "20px", marginBottom: "5px"}}>{exp.employer}</div>
-                                    <div style={{fontFamily: "'Quattrocento', serif", fontSize: "18px", marginBottom: "5px"}}>{exp.jobTitle}</div>
-                                    <div style={{fontFamily: "'Quattrocento', serif"}}>{exp.date}</div>
-                                </div>
-                                <div className="expCardBack" style={{backgroundColor: exp.employerImg.bkgColor}}>
-                                    <div style={{display: "flex", flexDirection: "row", alignContent: "center"}} className="rowCard">
-                                        <img style={{objectFit: "contain", width: "25%", margin: "20px"}} src={exp.employerImg.img} alt="" />
-                                        <div style={{display: "flex", flexDirection: "column", margin: "20px 0px", alignSelf: "center"}}>
-                                            <div style={{fontFamily: "'Abril Fatface', cursive", fontSize: "18px", marginBottom: "5px", textAlign: "left"}}>{exp.employer}</div>
-                                            <div style={{fontFamily: "'Quattrocento', serif", fontSize: "16px", marginBottom: "5px", textAlign: "left"}}>{exp.jobTitle}</div>
-                                            <div style={{fontFamily: "'Quattrocento', serif", fontSize: "14px", textAlign: "left"}}>{exp.date}</div>
-                                        </div>
-                                    </div>
-                                    <ul>
-                                        {
-                                            exp.descs.map((desc) => (
-                                                <li style={exp.descs.length>=3 ? {fontFamily: "'Quattrocento', serif", marginBottom: "10px",textAlign: "left", fontSize: "16px"} : {fontFamily: "'Quattrocento', serif", marginBottom: "15px", textAlign: "left", fontSize: "18px"}}>{desc}</li>
-                                            ))
-                                        }
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                    ))
-                }
-            </div>
+//                     <p className="headerTitle" >Experience</p>
+//                 </div>
+//                 <div className="divider" style={{width: "255px"}}></div>
+//             </div>
+//             <div className="expContentContainer">
+//                 {
+//                     expData.map((exp) => (
+//                         <div className="expCard">
+//                             <div className="expCardContent">
+//                                 <div className="expCardFront">
+//                                     <img style={{objectFit: "contain", width: "55%", margin: "20px"}} src={exp.employerImg.img} alt="" />
+//                                     <div style={{fontFamily: "'Abril Fatface', cursive", fontSize: "20px", marginBottom: "5px"}}>{exp.employer}</div>
+//                                     <div style={{fontFamily: "'Quattrocento', serif", fontSize: "18px", marginBottom: "5px"}}>{exp.jobTitle}</div>
+//                                     <div style={{fontFamily: "'Quattrocento', serif"}}>{exp.date}</div>
+//                                 </div>
+//                                 <div className="expCardBack" style={{backgroundColor: exp.employerImg.bkgColor}}>
+//                                     <div style={{display: "flex", flexDirection: "row", alignContent: "center"}} className="rowCard">
+//                                         <img style={{objectFit: "contain", width: "25%", margin: "20px"}} src={exp.employerImg.img} alt="" />
+//                                         <div style={{display: "flex", flexDirection: "column", margin: "20px 0px", alignSelf: "center"}}>
+//                                             <div style={{fontFamily: "'Abril Fatface', cursive", fontSize: "18px", marginBottom: "5px", textAlign: "left"}}>{exp.employer}</div>
+//                                             <div style={{fontFamily: "'Quattrocento', serif", fontSize: "16px", marginBottom: "5px", textAlign: "left"}}>{exp.jobTitle}</div>
+//                                             <div style={{fontFamily: "'Quattrocento', serif", fontSize: "14px", textAlign: "left"}}>{exp.date}</div>
+//                                         </div>
+//                                     </div>
+//                                     <ul>
+//                                         {
+//                                             exp.descs.map((desc) => (
+//                                                 <li style={exp.descs.length>=3 ? {fontFamily: "'Quattrocento', serif", marginBottom: "10px",textAlign: "left", fontSize: "16px"} : {fontFamily: "'Quattrocento', serif", marginBottom: "15px", textAlign: "left", fontSize: "18px"}}>{desc}</li>
+//                                             ))
+//                                         }
+//                                     </ul>
+//                                 </div>
+//                             </div>
+//                         </div>
+//                     ))
+//                 }
+//             </div>
+//         </div>
+//     )
+// }
+
+// export default Exp;
+import React from "react";
+
+
+function Exp() {
+  return (
+    <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-gray-800 mb-4">Tailwind CSS Component Experience Page</h1>
+      <div className="flex flex-wrap justify-center">
+        <div className="w-72 h-72 bg-white shadow-lg rounded-lg overflow-hidden mx-4 my-8 transform hover:-translate-y-1 transition duration-500 ease-in-out">
+          <img src="https://source.unsplash.com/random/800x800" alt="" className="w-full h-44 object-cover" />
+          <div className="p-6">
+            <h2 className="text-lg font-bold text-gray-800 mb-2">Product Name</h2>
+            <p className="text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at purus sit amet ligula lacinia sollicitudin ac vel elit. Aliquam fermentum magna eget est aliquet, ut vehicula magna accumsan. </p>
+            <button className="mt-4 bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full transition duration-300 ease-in-out">Buy Now</button>
+          </div>
         </div>
-    )
+        <div className="w-72 h-72 bg-white shadow-lg rounded-lg overflow-hidden mx-4 my-8 transform hover:-translate-y-1 transition duration-500 ease-in-out">
+          <img src="https://source.unsplash.com/random/800x800" alt="" className="w-full h-44 object-cover" />
+          <div className="p-6">
+            <h2 className="text-lg font-bold text-gray-800 mb-2">Product Name</h2>
+            <p className="text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at purus sit amet ligula lacinia sollicitudin ac vel elit. Aliquam fermentum magna eget est aliquet, ut vehicula magna accumsan. </p>
+            <button className="mt-4 bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full transition duration-300 ease-in-out">Buy Now</button>
+          </div>
+        </div>
+        <div className="w-72 h-72 bg-white shadow-lg rounded-lg overflow-hidden mx-4 my-8 transform hover:-translate-y-1 transition duration-500 ease-in-out">
+          <img src="https://source.unsplash.com/random/800x800" alt="" className="w-full h-44 object-cover" />
+          <div className="p-6">
+            <h2 className="text-lg font-bold text-gray-800 mb-2">Product Name</h2>
+            <p className="text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at purus sit amet ligula lacinia sollicitudin ac vel elit. Aliquam fermentum magna eget est aliquet, ut vehicula magna accumsan. </p>
+            <button className="mt-4 bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full transition duration-300 ease-in-out">Buy Now</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Exp;
--- src/components/Portfolio/Project/ProjectCard.js
+++ src/components/Portfolio/Project/ProjectCard.js
@@ -9,18 +9,20 @@ Shark.propTypes = {
 export default function Shark({ item }) {
   console.log(item);
   return (
-    <section className="max-w-md w-[450px] border rounded-lg bg-white p-4 shadow-lg ring-gray-200 overflow-hidden backdrop-blur-[3px]">
+    
+    <section className="max-w-md w-[450px] border rounded-lg bg-white p-4 shadow-lg ring-gray-200 overflow-hidden hover:scale-105 transition duration-500  backdrop-blur-[3px]">
+     <Link to="/pages">
     <div className="h-2/4 bg-blue-100 overflow-hidden object-cover">
-      <Link to="/pages">
-        <img className="w-full h-full origin-center" src={item.image} />
-      </Link>
+     
+    <img className="w-full h-60 origin-center" src={item.image} />
+      
     </div>
     <div className="p-3 h-2/4 overflow-hidden text-clip">
       <h5 className="mb-1 text-xl font-semibold tracking-wide">{item.name}</h5>
       <p className="h-[100px] font-normal text-sm text-black text-ellipsis overflow-hidden tracking-wide leading-normal">
         {item.description}
       </p>
-    </div>
+    </div></Link>
   </section>
   );
 }
